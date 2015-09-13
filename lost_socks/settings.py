ccc00cf9a771468eb6f6371b954e39ee3c8da9c3
Ariet <YOUR@EMAIL.com>
2015-09-13 07:51:07 +0600

    dadad

--- lost_socks/settings.py
+++ lost_socks/settings.py
@@ -90,24 +90,4 @@ STATIC_DIRS = (
 )
 
 STATIC_ROOT = os.path.join(BASE_DIR,'static-root')
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR,'media')
-
-# djagno all authorization 
-TEMPLATE_CONTEXT_PROCESSORS = (
-    'django.core.context_processors.request',
-    'django.contrib.auth.context_processors.auth',
-)
-
-AUTHENTICATION_BACKENDS = (
-    # Needed to login by username in Django admin, regardless of `allauth`
-    'django.contrib.auth.backends.ModelBackend',
-
-    # `allauth` specific authentication methods, such as login by e-mail
-    'allauth.account.auth_backends.AuthenticationBackend',
-)
-
-SITE_ID = 1
-LOGIN_REDIRECT_URL = "/"
-
 
