8903ff7a0a124b42454d92ca0a7cbaff58c9611e
Ariet <YOUR@EMAIL.com>
2015-09-13 07:45:46 +0600

    yahoo

--- main/models.py
+++ main/models.py
@@ -5,16 +5,15 @@ from django.contrib.auth.models import User
 class Cardstick(models.Model):
 	""" Card info """
 
-	card_id = models.CharField(max_length = 7)
+	card_id = models.CharField(max_length = 7, unique=True)
 	activated = models.BooleanField(default = False)
-	telephone = models.CharField(max_length = 20,default = "")
-	email = models.CharField(max_length = 50,default = "")
-	name = models.CharField(max_length = 100,default = "")
+	telephone = models.CharField(max_length = 20,default = "",null=True, blank=True)
+	email = models.CharField(max_length = 50,default = "",null=True, blank=True)
+	name = models.CharField(max_length = 100,default = "",null=True, blank=True)
 
 	def __unicode__(self):
 		return self.card_id
 
 	class Meta:
-		verbose_name = "каrta"
-		verbose_name_plural = "каrty"
-
+		verbose_name = "Карта"
+		verbose_name_plural = "Карты"
